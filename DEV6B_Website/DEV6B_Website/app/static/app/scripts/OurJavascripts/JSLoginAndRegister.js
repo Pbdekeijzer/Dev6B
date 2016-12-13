ea916c6f1e93854d42bdb71223e8b04a971fbc62
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-12-13 15:29:16 +0100

    JQuery ready for use

---
+++ DEV6B_Website/DEV6B_Website/app/static/app/scripts/OurJavascripts/JSLoginAndRegister.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#LoginButton").click(function () {
+        $("#LoginButton").attr('value', 'It worked');
+    });
+
+    $("#RegisterButton").click(function () {
+        $("#RegisterButton").attr('value', 'It Worked');
+    });
+
+});
