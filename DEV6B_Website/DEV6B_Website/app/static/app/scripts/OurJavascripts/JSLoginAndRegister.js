54a21b3895cbea69d4cfecfe87f5cf4a68bb87f6
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-12-15 11:42:51 +0100

    Doodlopend, reverted

--- DEV6B_Website/DEV6B_Website/app/static/app/scripts/OurJavascripts/JSLoginAndRegister.js
+++ DEV6B_Website/DEV6B_Website/app/static/app/scripts/OurJavascripts/JSLoginAndRegister.js
@@ -1,10 +1,18 @@
 ﻿$(document).ready(function () {
     $("#LoginButton").click(function () {
-        $("#LoginButton").attr('value', 'It worked');
+        $("#LoginButton").attr("value", "It worked");
     });
 
+
     $("#RegisterButton").click(function () {
-        $("#RegisterButton").attr('value', 'It Worked');
+
+
+
     });
+
+
+
+
+
 
 });
--- DEV6B_Website/DEV6B_Website/manage.py
+++ DEV6B_Website/DEV6B_Website/manage.py
@@ -6,6 +6,8 @@ Command-line utility for administrative tasks.
 import os
 import sys
 
+
+
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
@@ -14,4 +16,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
