9cfeaff63d12cc1a0a4042516347c9abb7df4281
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-12-15 11:07:40 +0100

    database connnection
    database connnection

--- DEV6B_Website/DEV6B_Website/Database.py
+++ DEV6B_Website/DEV6B_Website/Database.py
@@ -2,8 +2,14 @@ import sqlite3
 import sys
 
 def Execute_Query(query):
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.execute(query)
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('db.sqlite3')
+        c = conn.cursor()
+        c.execute(query)
+        ret = c.fetchall()
+        conn.commit()
+        conn.close()
+        return ret
+    except:
+        print("Execute failed!")
+        return 400
--- DEV6B_Website/DEV6B_Website/app/models.py
+++ DEV6B_Website/DEV6B_Website/app/models.py
@@ -3,5 +3,13 @@ Definition of models.
 """
 
 from django.db import models
+import Database
 
 # Create your models here.
+def GetGameUserData(username):
+    Query = "SELECT * FROM Game_User"# WHERE Name = '{0}'".format(str(username))
+    print(Query)
+    result = Database.Execute_Query(Query)
+    print(result)
+
+GetGameUserData('username')
