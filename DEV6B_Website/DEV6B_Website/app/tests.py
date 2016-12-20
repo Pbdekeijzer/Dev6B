f526b8bb79a15ca3dc44f2a708da063e0d90e3db
ChielBroere <chielbroere@gmail.com>
2016-12-20 13:14:21 +0100

    More tests

--- DEV6B_Website/DEV6B_Website/app/tests.py
+++ DEV6B_Website/DEV6B_Website/app/tests.py
@@ -6,6 +6,7 @@ when you run "manage.py test".
 import django
 from django.test import TestCase
 
+
 # TODO: Configure your database in settings.py and sync before running tests.
 
 class ViewTest(TestCase):
@@ -18,20 +19,21 @@ class ViewTest(TestCase):
             super(ViewTest, cls).setUpClass()
             django.setup()
 
-    def test_home(self):
-        """Tests the home page."""
-        response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
-
     def test_truetest(self):
         test = True
         self.assertTrue(test)
 
     def test_falsetest(self):
         test = False
-        self.assertTrue(test)
+        self.assertFalse(test)
     
     def test_lasttest(self):
         test1 = 5
         test2 = 5        
         self.assertEqual(test1, test2)
+    
+    def test_somemoretests(self):
+        hoi = "lol"
+        self.assertEqual(hoi, "lol")
+
+    
