d462cf3beb36d447abce21ea323fa4202a1c5c51
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2017-01-03 10:20:38 +0100

    All available jobs are now put in the html by javascript
    That means one step closer to getting it from the database

---
+++ DEV6B_Website/DEV6B_Website/app/static/app/scripts/OurJavascripts/about.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+
+
+    for (var index = 1; index < 20; index++) {
+
+        $("#AllJobs").append("<div class='well'><p>Job"+index+"</p><p><a href='#' class='btn btn-primary btn-large'>Select Job "+index+"</a></p></div>");
+
+        
+    }
+
+
+
+
+
+    $('.well').click(function () {
+        var id = $(this).attr('id');
+        if (id == "m1") {
+            //do your stuff here
+        }
+
+
+
+    });
+
+
+
+
+});
