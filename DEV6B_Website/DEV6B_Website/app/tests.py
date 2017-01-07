0cb171a09ce9c00076460cb1dabab45f060fc901
ChielBroere <chielbroere@gmail.com>
2017-01-07 13:03:43 +0100

    New tests

--- DEV6B_Website/DEV6B_Website/app/tests.py
+++ DEV6B_Website/DEV6B_Website/app/tests.py
@@ -18,19 +18,37 @@ class ViewTest(TestCase):
             super(ViewTest, cls).setUpClass()
             django.setup()
 
-    def test_home(self):
-        """Tests the home page."""
-        response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
+class TemplateTest(TestCase):        
+    def test_templateindex(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, "app/index.html")
 
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+    def test_templateabout(self):
+        client = Client()
+        response = client.get('/about')
+        self.assertTemplateUsed(response, "app/about.html")
 
-    #def test_about(self):
-    #    """Tests the about page."""
-    #    response = self.client.get('/about')
-    #    self.assertContains(response, 'About', 3, 200)
+    def test_templatelogin(self):
+        client = Client()
+        response = client.get('/login')
+        self.assertTemplateUsed(response, "app/login.html")
 
-#this is a test ???!
+class ResponseTest(TestCase):           
+    def test_responseindex(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_responseabout(self):
+        client = Client()
+        response = client.get('/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_responselogin(self):
+        client = Client()
+        response = client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+#These are the tests
+
