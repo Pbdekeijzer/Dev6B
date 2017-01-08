f7d26019bde02f082752078d683757d832f6334f
ChielBroere <chielbroere@gmail.com>
2017-01-08 11:50:42 +0100

    Added django client to tests

--- DEV6B_Website/DEV6B_Website/app/tests.py
+++ DEV6B_Website/DEV6B_Website/app/tests.py
@@ -5,6 +5,7 @@ when you run "manage.py test".
 
 import django
 from django.test import TestCase
+from django.test import Client
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
@@ -18,37 +19,37 @@ class ViewTest(TestCase):
             super(ViewTest, cls).setUpClass()
             django.setup()
 
-#class TemplateTest(TestCase):        
-#    def test_templateindex(self):
-#        client = Client()
-#        response = client.get('/')
-#        self.assertTemplateUsed(response, "app/index.html")
+class TemplateTest(TestCase):        
+    def test_templateindex(self):
+        client = Client()
+        response = client.get('/')
+        self.assertTemplateUsed(response, "app/index.html")
 
-#    def test_templateabout(self):
-#        client = Client()
-#        response = client.get('/about')
-#        self.assertTemplateUsed(response, "app/about.html")
+    def test_templateabout(self):
+        client = Client()
+        response = client.get('/about')
+        self.assertTemplateUsed(response, "app/about.html")
 
-#    def test_templatelogin(self):
-#        client = Client()
-#        response = client.get('/login')
-#        self.assertTemplateUsed(response, "app/login.html")
+    def test_templatelogin(self):
+        client = Client()
+        response = client.get('/login')
+        self.assertTemplateUsed(response, "app/login.html")
 
-#class ResponseTest(TestCase):           
-#    def test_responseindex(self):
-#        client = Client()
-#        response = client.get('/')
-#        self.assertEqual(response.status_code, 200)
+class ResponseTest(TestCase):           
+    def test_responseindex(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
 
-#    def test_responseabout(self):
-#        client = Client()
-#        response = client.get('/about')
-#        self.assertEqual(response.status_code, 200)
+    def test_responseabout(self):
+        client = Client()
+        response = client.get('/about')
+        self.assertEqual(response.status_code, 200)
 
-#    def test_responselogin(self):
-#        client = Client()
-#        response = client.get('/login')
-#        self.assertEqual(response.status_code, 200)
+    def test_responselogin(self):
+        client = Client()
+        response = client.get('/login')
+        self.assertEqual(response.status_code, 200)
 
-##These are the tests
+#These are the tests
 
